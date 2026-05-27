a0efdd6d4f580c5bc9765ec61e2ad9860f28ca26
altarialights <altarialights@gmail.com>
2026-05-27 17:29:25 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+    site: "https://altarialights.com",
+    integrations: [react(), sitemap()],
     vite: {
-        plugins: [tailwindcss()],
+        plugins: [/** @type {any} */ (tailwindcss())],
     },
 });
